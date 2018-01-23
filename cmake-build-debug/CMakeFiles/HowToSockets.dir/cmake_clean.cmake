893ab723c8ebd36d438218202eeb9e3110ffcb74
Paul Thompson <pbt@pdx.edu>
2018-01-23 02:55:40 -0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/HowToSockets.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HowToSockets.dir/main.c.o"
+  "HowToSockets.pdb"
+  "HowToSockets.exe"
+  "libHowToSockets.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HowToSockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
